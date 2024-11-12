53d5d589ac2be34ada029c73becb56c76e01965d
Krishna28-del <gopalakrishna2806@gmail.com>
2024-11-12 21:28:17 +0530

    Second commit

--- mlops/settings.py
+++ mlops/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1nl5)th4z19l5wz%xy_09jtyy)ff9!u^78tq*xz8si3pcao47a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Mlops.onrender.com','127.0.0.1']
 
 
 # Application definition
